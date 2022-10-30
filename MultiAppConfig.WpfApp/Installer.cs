a2b1654f14af25b0600c18fbc63482fb5ceebb7a
dev <dev@dev.org>
2022-10-30 11:24:24 +0100

    add simple setup with custom actions

---
+++ MultiAppConfig.WpfApp/Installer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiAppConfig.WpfApp
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+#if DEBUG
+            Debugger.Launch();
+#endif
+
+            base.OnAfterInstall(savedState);
+        }
+    }
+}
